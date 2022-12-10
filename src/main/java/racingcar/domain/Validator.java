153535372f725e9e3ca590acad8038b8c3eb866c
khyojun <nandong5923@naver.com>
2022-12-10 11:02:39 +0900

    feat(racingcar): ,가 구분자가 아닌 경우에 대한 예외처리 기능 추가

--- src/main/java/racingcar/domain/Validator.java
+++ src/main/java/racingcar/domain/Validator.java
@@ -9,7 +9,9 @@ public class Validator {
 
     public String validateCarName(String readCarName) {
         List<String> split = List.of(readCarName.split(","));
-
+        if(readCarName.chars().filter(n -> n==',').count() != split.size()-1){
+            throw new IllegalArgumentException("[ERROR] 구분자로는 , 가 와야 합니다.");
+        }
         List<Car> collect = split.stream().map(s -> new Car(s)).collect(Collectors.toList());
 
 
