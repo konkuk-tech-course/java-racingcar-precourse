6ae9409364d794a330fbef36f318df082338c744
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 11:11:47 +0900

    fix(Game): cars 필드의 자료형 Map으로 수정

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -3,15 +3,17 @@ package racingcar.domain;
 import constant.Constants;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Game {
     private int moveCount;
-    private List<Car> cars;
+    private Map<String, Car> cars;
 
     public Game() {
         moveCount = Constants.INIT_VALUE.get();
-        cars = new ArrayList<>();
+        cars = new HashMap<>();
     }
     }
 }
