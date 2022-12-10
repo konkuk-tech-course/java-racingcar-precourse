ada135abc72007f7264d0f1dbb8f37f4fe8ccc35
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 13:07:11 +0900

    feat(GameService): 1번 이동을 진행하는 기능 구현

--- src/main/java/racingcar/service/GameService.java
+++ src/main/java/racingcar/service/GameService.java
@@ -1,10 +1,35 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.domain.Game;
+import racingcar.service.constant.MoveConditionProperties;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class GameService {
     private static class InstanceHolder {
         private static final GameService INSTANCE = new GameService();
     }
+
     public static GameService getInstance() {
         return InstanceHolder.INSTANCE;
     }
+
+    public void playRound(Game game) {
+        Map<String, Boolean> moveConditions = new HashMap<>();
+        game.getAllCars().forEach(name -> moveConditions.put(name, isMovable()));
+        game.controlCars(moveConditions);
+    }
+
+    private boolean isMovable() {
+        int randomValue = Randoms.pickNumberInRange(
+                MoveConditionProperties.MIN.get(),
+                MoveConditionProperties.MAX.get()
+        );
+        return randomValue >= MoveConditionProperties.MIN_MOVABLE.get();
+    }
+
 }
