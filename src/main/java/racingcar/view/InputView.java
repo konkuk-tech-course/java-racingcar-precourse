594378a7e8503a298f2d0341ccb25b8c47c886f5
Life is a ninanino <tvxqinak@naver.com>
2022-12-10 15:27:40 +0900

    refactor : 사용자 입력기능 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,20 +10,22 @@ import racingcar.view.constant.ViewMessage;
  */
 public class InputView {
 
-    public String input() {
-        return Console.readLine();
-    }
 
     public String[] inputCarName(){
         System.out.println(ViewMessage.INPUT_GAME_START_MESSAGE.getMessage());
-        String carName[] = input().split(",");
-        Validator.validateInputName(input());
+        String input = Console.readLine();
+        String carName[] = input.split(",");
+        for(int i=0; i<carName.length; i++){
+            String name = carName[i];
+            carName[i] = name.trim();
+        }
+        //Validator.validateInputName(input);
         return carName;
     }
 
     public int inputTryCount(){
         System.out.println(ViewMessage.INPUT_USER_TRY_COUNT.getMessage());
-        int count = Integer.parseInt(input());
+        int count = Integer.parseInt(Console.readLine());
         Validator.validateInputTryNumber(count);
         return count;
     }
