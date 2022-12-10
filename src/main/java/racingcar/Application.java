72a00d780aa9daeb8ae9401d79c6ead7d0207975
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 14:38:50 +0900

    feat(Application): 메인 메서드 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.domain.Game;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        final GameController gameController = GameController.getInstance();
+        Game game = gameController.createGame();
+        gameController.operate(game);
     }
 }
