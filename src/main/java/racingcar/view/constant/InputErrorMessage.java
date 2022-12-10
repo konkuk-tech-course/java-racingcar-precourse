a39b47280ed3e4c4e41e37f1c2731087e2f9dba1
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 13:34:01 +0900

    feat(InputErrorMessage): 올바르지 않은 이름길이에 대한 에러메세지 추가

--- src/main/java/racingcar/view/constant/InputErrorMessage.java
+++ src/main/java/racingcar/view/constant/InputErrorMessage.java
@@ -3,6 +3,7 @@ package racingcar.view.constant;
 public enum InputErrorMessage {
     EMPTY("빈 값을 입력했습니다."),
     EMPTY_VALUE_IN_NAMES("입력된 값들 중 빈 값이 포함되어 있습니다."),
+    INVALID_NAME_LENGTH("자동차 이름은 " + InputLimits.NAME_LENGTH.get() + "자 이하로 입력해주세요."),
     NOT_NUMBER("숫자만 입력해주세요");
 
     private static final String PREFIX = "[ERROR] ";
