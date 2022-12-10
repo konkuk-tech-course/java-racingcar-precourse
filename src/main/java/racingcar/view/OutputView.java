d8d8df46c51ceb19b6fb8ec7fe0d438724dde3e3
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 14:04:04 +0900

    feat(OutputView): 결과 출력 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,9 @@
 package racingcar.view;
 
+import racingcar.view.constant.OutputStyles;
+
+import java.util.Map;
+
 public class OutputView {
     private static class InstanceHolder {
         private static final OutputView INSTANCE = new OutputView();
@@ -8,5 +12,22 @@ public class OutputView {
     public static OutputView getInstance() {
         return OutputView.InstanceHolder.INSTANCE;
     }
-    
+
+    public void printResult(Map<String, Integer> results) {
+        results.entrySet().stream()
+                .map(this::styleResult)
+                .forEach(System.out::println);
+    }
+
+    private String styleResult(Map.Entry<String, Integer> result) {
+        return String.join(
+                OutputStyles.RESULT_DELIMITER.get(),
+                result.getKey(),
+                OutputStyles.MOVE.get().repeat(result.getValue())
+        );
+    }
+
+    public void printWinners() {
+
+    }
 }
