464e76982945f38ebb8f8f04d9382294827a75e7
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 14:21:04 +0900

    feat(GameController): 게임 생성 컨트롤러 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,13 +1,46 @@
 package racingcar.controller;
 
+import racingcar.domain.Game;
+import racingcar.service.GameService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class GameController {
+    private static InputView inputView = InputView.getInstance();
+    private static OutputView outputView = OutputView.getInstance();
+    private static GameService gameService = GameService.getInstance();
+
     private static class InstanceHolder {
         private static final GameController INSTANCE = new GameController();
     }
 
     public static GameController getInstance() {
         return GameController.InstanceHolder.INSTANCE;
+    }
+
+    public Game createGame() {
+        List<String> names = requestNames();
+        int tryCount = requestTryCount();
+        return gameService.createGame(names, tryCount);
+    }
+
+    private List<String> requestNames() {
+        try {
+            return inputView.readNames();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return requestNames();
+        }
+    }
+
+    private int requestTryCount() {
+        try {
+            return inputView.readTryCount();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return requestTryCount();
+        }
     }
 }
