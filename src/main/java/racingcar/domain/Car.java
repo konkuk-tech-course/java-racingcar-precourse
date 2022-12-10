5e4b975c41d8ccf6bb19179932488228e8ca4c91
skydreamer21 <skydreamer210@gmail.com>
2022-12-10 10:54:02 +0900

    feat(Car): 전진 메서드 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,18 @@
 package racingcar.domain;
 
 public class Car {
+    private static final int UNIT_DISTANCE = 1;
+
     private String name;
     private int distance;
-
+    
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+
+    public void move() {
+        distance += UNIT_DISTANCE;
     }
 
     public String getName() {
